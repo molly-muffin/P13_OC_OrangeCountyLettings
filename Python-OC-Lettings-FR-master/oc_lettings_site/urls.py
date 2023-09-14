14ef7f7b9f61b001f8865912b8a274054787af47
Molly Muffin <laureendademeule@gmail.com>
2023-09-14 11:44:04 +0200

    split appli

---
+++ Python-OC-Lettings-FR-master/oc_lettings_site/urls.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('', include(('home.urls', 'home'), namespace='home')),
+    path('lettings/', include(('lettings.urls', 'lettings'), namespace='lettings')),
+    path('profiles/', include(('profiles.urls', 'profiles'), namespace='profiles')),
+    path('admin/', admin.site.urls),
+]
