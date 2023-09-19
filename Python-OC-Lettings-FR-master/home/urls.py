cc4c1635f24cfd5f46ef37a0cabb3e3529a38673
Molly Muffin <laureendademeule@gmail.com>
2023-09-19 13:32:54 +0200

    merge

--- Python-OC-Lettings-FR-master/home/urls.py
+++ Python-OC-Lettings-FR-master/home/urls.py
@@ -1,11 +1,8 @@
 from django.urls import path
-from .views import index
+from home.views import index
 
 
-# Namespace pour les URLs de l'application 'home'
 app_name = 'home'
-
 urlpatterns = [
-    # URL de la page d'accueil
     path('', index, name='index'),
 ]
